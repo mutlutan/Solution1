610e59762ceaf10c2fdcd8761b7ddb65ec4e7f8c
Mutlu MUTLUTAN <mutlutan@outlook.com>
2023-08-08 12:23:35 +0300

    portal ekleniyor

---
+++ WebApp.Portal/Codes/Models.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApp.Portal.Codes
+{
+    public class AppConfig
+    {
+        public string MainConnection { get; set; } = "";
+        public string LogConnection { get; set; } = "";
+    }
+}
--- WebApp.Portal/Program.cs
+++ WebApp.Portal/Program.cs
@@ -1,7 +1,30 @@
+using AppCommon;
+
 var builder = WebApplication.CreateBuilder(args);
+//linux için proxy adresi
+//builder.WebHost.UseUrls("http://localhost:5001/");
+
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddMemoryCache();  //Aktif
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddControllers().AddNewtonsoftJson();
+// newtonsoft ise
+builder.Services.AddControllersWithViews().AddNewtonsoftJson(options =>
+{
+    //options.SerializerSettings.Converters.Add(new NetTopologySuite.IO.Converters.GeometryConverter());
+    options.SerializerSettings.ContractResolver = new Newtonsoft.Json.Serialization.DefaultContractResolver(); // Use the default property (Pascal) casing
+}
+).AddJsonOptions((options) =>
+{
+    //options.JsonSerializerOptions.Converters.Add(new NetTopologySuite.IO.Converters.GeometryConverter());
+    options.JsonSerializerOptions.PropertyNamingPolicy = null; // system json ise camle case ayarý
+}
+);
+
+builder.Services.AddBrowserDetection(); //Install-Package Shyjus.BrowserDetector
 
 var app = builder.Build();
 
@@ -25,3 +48,4 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
