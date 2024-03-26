e0c5681010739ee96f36d140786ef8fe411f976e
Mutlu MUTLUTAN <mutlutan@outlook.com>
2024-03-27 02:23:46 +0300

    WebApp.Portal silindi

--- WebApp.Portal/Codes/Models.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WebApp.Portal.Codes
-{
-    public class AppConfig
-    {
-        public string MainConnection { get; set; } = "";
-        public string LogConnection { get; set; } = "";
-    }
-}
